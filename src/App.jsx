f0d2e4e8a5f66a84dcd6050265d63fae26f1790e
Shlok Rastogi <shlokrastogi2004@gmail.com>
2026-04-23 00:26:13 +0530

    temp commit before cleaning history

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import Login from "./components/Login";
 import Profile from "./components/Profile";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
 
 function App() {
   return (
     <Provider store={appStore}>
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body />}>
+            <Route path="/" element={<Feed />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
           </Route>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { addUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:3000/login",
+        `${BASE_URL}/login`,
         {
           email: emailId,
           password,
         },
         {
-          withCredentials: true, // 🔥 REQUIRED
+          withCredentials: true,
         },
       );
+      dispatch(addUser(res.data.user));
+      return navigate("/");
     } catch (err) {
       console.error("Login failed:", err);
     }
